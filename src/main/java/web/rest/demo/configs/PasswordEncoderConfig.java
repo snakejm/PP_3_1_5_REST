c30f1b0be10a46c810fd4e93da902643a549083e
snake <snakejedymaster@gmail.com>
2023-10-28 02:19:10 +0530

    Перенес PasswordEncoder

---
+++ src/main/java/web/rest/demo/configs/PasswordEncoderConfig.java
@@ -0,0 +1,14 @@
+package web.rest.demo.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class PasswordEncoderConfig {
+    @Bean
+    protected PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(12);
+    }
+}
